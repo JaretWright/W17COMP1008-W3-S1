21d3e0fafc754b65adb5bd62896579a68027c5e5
jwright <jwright@10.250.18.81>
2017-02-14 17:42:25 -0500

    Added radio buttons, toggle group and change listener

--- src/w17jan24/CreateNewEmployeeViewController.java
+++ src/w17jan24/CreateNewEmployeeViewController.java
@@ -7,11 +7,17 @@ package w17jan24;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -26,18 +32,34 @@ public class CreateNewEmployeeViewController implements Initializable {
     @FXML private DatePicker dobPicker;
     @FXML private Button createNewEmployeeButton;
     
+    @FXML private RadioButton hourlyRadioButton;
+    @FXML private RadioButton commissionedRadioButton;
+    @FXML private Label errorMessageLabel;
+    @FXML private Label payRateLabel;
+    
     /**
      * This will create a new HourlyEmployee when the button is pushed
      */
     public void createNewEmployeeButtonPushed()
     {
-        HourlyEmployee newEmployee = new HourlyEmployee(firstNameTextField.getText(),
-                                                        lastNameTextField.getText(),
-                                                        socialInsuranceNumTextField.getText(),
-                                                        dobPicker.getValue(),
-                                         Double.parseDouble(hourlyRateTextField.getText()));
-        
-        System.out.printf("The new employee is %s%n", newEmployee);
+        if (this.hourlyRadioButton.isSelected())
+        {
+            try
+            {
+                HourlyEmployee newEmployee = new HourlyEmployee(firstNameTextField.getText(),
+                                                            lastNameTextField.getText(),
+                                                            socialInsuranceNumTextField.getText(),
+                                                            dobPicker.getValue(),
+                                             Double.parseDouble(hourlyRateTextField.getText()));
+
+                System.out.printf("The new employee is %s%n", newEmployee);
+            }
+            catch (IllegalArgumentException e)
+            {
+                this.errorMessageLabel.setText(e.getMessage());
+            }
+        }
+    
     }
     
     
@@ -46,7 +68,35 @@ public class CreateNewEmployeeViewController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        ToggleGroup employeeTypeGroup = new ToggleGroup();
+        this.hourlyRadioButton.setToggleGroup(employeeTypeGroup);
+        this.commissionedRadioButton.setToggleGroup(employeeTypeGroup);
+        this.errorMessageLabel.setVisible(false);  //makes it invisible
+        this.hourlyRadioButton.setSelected(true);
+        
+        employeeTypeGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Toggle>  observable, 
+                                Toggle oldValue, Toggle newValue)
+            {
+                if (employeeTypeGroup.getSelectedToggle() != null)
+                {
+                    //configure the view for an hourly employee
+                    if (hourlyRadioButton.isSelected())
+                    {
+                        payRateLabel.setText("Hourly Rate");
+                        hourlyRateTextField.setPromptText("Hourly Rate");
+                    }
+                    
+                    else if (commissionedRadioButton.isSelected())
+                    {
+                        payRateLabel.setText("Commission Rate");
+                        hourlyRateTextField.setPromptText("Commission Rate");
+                    }
+                }
+            }
+            
+        });
     }    
-    
 }
